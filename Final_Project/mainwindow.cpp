5c0dcf18735dab007b83fa5745bf65758b54e03e
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-10 04:02:30 +0430

    Rename not_found to tempB & end changeResolution & rm message Done

--- Final_Project/mainwindow.cpp
+++ Final_Project/mainwindow.cpp
@@ -3,7 +3,7 @@
 #include "QFileDialog"
 #include "qmessagebox.h"
 #include <QInputDialog>
-
+#include <iostream>
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
@@ -35,21 +35,21 @@ void MainWindow::on_convertFormat_clicked()
     QString formats = "*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wv *.8svx *.cda)";
     QMessageBox * msg = new QMessageBox();
     msg->setWindowTitle("Convert video and audio formats");
-    QMessageBox * not_found = new QMessageBox();
-    not_found->setWindowTitle("Convert video and audio formats");
+    QMessageBox * tempB = new QMessageBox();
+    tempB->setWindowTitle("Convert video and audio formats");
     msg->setFont(font);
-    not_found->setIcon(QMessageBox::Icon::Critical);
-    not_found->setFont(font);
+    tempB->setIcon(QMessageBox::Icon::Critical);
+    tempB->setFont(font);
     bool fOk;
     QString format = QInputDialog::getText(this, tr("Output format"), tr("Format : "), QLineEdit::Normal, "Example: mp3", &fOk);
     if (!fOk)
     {
         delete msg;
-        delete not_found;
+        delete tempB;
         return;
     }
     QPushButton * input = new QPushButton(), * output = new QPushButton(), * ok = new QPushButton(), * Cancel = new QPushButton();
-    ok = not_found->addButton((tr("Ok")), QMessageBox::ActionRole);
+    ok = tempB->addButton((tr("Ok")), QMessageBox::ActionRole);
     ok->setShortcut(Qt::CTRL + Qt::Key_K);
     ok->setToolTip("Ctrl+K");
     QString fileName, outputFolder;
@@ -78,25 +78,25 @@ void MainWindow::on_convertFormat_clicked()
         }
         else if(msg->clickedButton() == output)
         {
-            outputFolder = QFileDialog::getExistingDirectory(0, ("Select Output Folder"), QDir::currentPath());
+            outputFolder = QFileDialog::getExistingDirectory(this, ("Select Output Folder"), QDir::currentPath());
             goto click;
         }
         else if(msg->clickedButton() == ok)
         {
             if(fileName.size() == 0 && outputFolder.size() == 0)
             {
-                not_found->setText("File and path not selected.");
-                not_found->exec();
+                tempB->setText("File and path not selected.");
+                tempB->exec();
             }
             else if(outputFolder.size() == 0)
             {
-                not_found->setText("No route selected.");
-                not_found->exec();
+                tempB->setText("No route selected.");
+                tempB->exec();
             }
             else if(fileName.size() == 0)
             {
-                not_found->setText("No file selected.");
-                not_found->exec();
+                tempB->setText("No file selected.");
+                tempB->exec();
             }
             else
             {
@@ -109,8 +109,8 @@ void MainWindow::on_convertFormat_clicked()
                     QByteArray temp2 = temp.toLocal8Bit();
                     const char * t2 = temp2.data();
                     system(t2);
-                    not_found->setText("Done.");
-                    not_found->exec();
+                    tempB->setText("Done.");
+                    tempB->exec();
                 }
                 else
                 {
@@ -120,8 +120,6 @@ void MainWindow::on_convertFormat_clicked()
                     QByteArray temp2 = temp.toLocal8Bit();
                     const char * t2 = temp2.data();
                     system(t2);
-                    not_found->setText("Done.");
-                    not_found->exec();
                 }
             }
         }
@@ -135,7 +133,7 @@ void MainWindow::on_convertFormat_clicked()
         msg->setIcon(QMessageBox::Icon::Critical);
         msg->exec();
     }
-    delete not_found;
+    delete tempB;
     delete input;
     delete output;
     delete ok;
@@ -155,4 +153,78 @@ void MainWindow::on_changeResolution_clicked()
     {
         return;
     }
+    QFont font;
+    font.setBold(true);
+    font.setPointSize(15);
+    QMessageBox * msg = new QMessageBox();
+    msg->setWindowTitle("Change video resolution");
+    QMessageBox * tempB = new QMessageBox();
+    tempB->setWindowTitle("Change video resolution");
+    msg->setFont(font);
+    tempB->setIcon(QMessageBox::Icon::Critical);
+    tempB->setFont(font);
+    QPushButton * input = new QPushButton(), * output = new QPushButton(), * ok = new QPushButton(), * Cancel = new QPushButton();
+    ok = tempB->addButton((tr("Ok")), QMessageBox::ActionRole);
+    ok->setShortcut(Qt::CTRL + Qt::Key_K);
+    ok->setToolTip("Ctrl+K");
+    QString fileName, outputFolder;
+    msg->setIcon(QMessageBox::Icon::Information);
+    input = msg->addButton((tr("Input file")), QMessageBox::ActionRole);
+    input->setShortcut(Qt::CTRL + Qt::Key_O);
+    input->setToolTip("Ctrl+O");
+    output = msg->addButton((tr("Output folder")), QMessageBox::ActionRole);
+    output->setShortcut(Qt::CTRL + Qt::Key_F);
+    output->setToolTip("Ctrl+F");
+    ok = msg->addButton((tr("Ok")), QMessageBox::ActionRole);
+    ok->setShortcut(Qt::CTRL + Qt::Key_K);
+    ok->setToolTip("Ctrl+K");
+    Cancel = msg->addButton((tr("Cancel")), QMessageBox::ActionRole);
+    Cancel->setShortcut(Qt::CTRL + Qt::Key_Q);
+    Cancel->setToolTip("Ctrl+Q");
+    msg->setText("Please select the file and its storage location.");
+    click:
+    msg->exec();
+    if(msg->clickedButton() == input)
+    {
+        fileName = QFileDialog::getOpenFileName(this, tr("Open Video | Open Music"), "", tr("Video Files | Music Files(*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wv *.8svx *.cda)"));
+        goto click;
+    }
+    else if(msg->clickedButton() == output)
+    {
+        outputFolder = QFileDialog::getExistingDirectory(this, ("Select Output Folder"), QDir::currentPath());
+        goto click;
+    }
+    else if(msg->clickedButton() == ok)
+    {
+        if(fileName.size() == 0 && outputFolder.size() == 0)
+        {
+            tempB->setText("File and path not selected.");
+            tempB->exec();
+        }
+        else if(outputFolder.size() == 0)
+        {
+            tempB->setText("No route selected.");
+            tempB->exec();
+        }
+        else if(fileName.size() == 0)
+        {
+            tempB->setText("No file selected.");
+            tempB->exec();
+        }
+        else
+        {
+            QString temp = "ffmpeg -i ", name;
+            QFileInfo nameTemp(fileName);
+            name = nameTemp.fileName();
+            temp += fileName + " -s " + QString::number(width) + "x" + QString::number(height) + " -c:a copy " + outputFolder + "/Change_resolution_is_Basu_Video_Studio_" + name;
+            QByteArray temp2 = temp.toLocal8Bit();
+            const char * t2 = temp2.data();
+            system(t2);
+        }
+    }
+    delete tempB;
+    delete input;
+    delete output;
+    delete ok;
+    delete Cancel;
 }
