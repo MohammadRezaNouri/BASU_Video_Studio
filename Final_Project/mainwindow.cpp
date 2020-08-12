a7592b2320fb64d453ce7f56bd6bf63adcc7f449
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-12 20:47:40 +0430

    Second Box Message Terms in addRemoveAudio

--- Final_Project/mainwindow.cpp
+++ Final_Project/mainwindow.cpp
@@ -236,7 +236,7 @@ void MainWindow::on_addRemoveAudio_clicked()
     font.setPointSize(15);
     QMessageBox * one = new QMessageBox(), * temp = new QMessageBox(), * two = new QMessageBox();
     one->setWindowTitle("Add audio to video or remove audio from it");
-    QPushButton * addAudio = new QPushButton(), * rmAudio = new QPushButton(), * ok = new QPushButton(), * Cancel = new QPushButton(),* input = new QPushButton(), * output = new QPushButton();
+    QPushButton * addAudio = new QPushButton(), * rmAudio = new QPushButton(), * ok = new QPushButton(), * Cancel = new QPushButton(),* inputV = new QPushButton(), * output = new QPushButton(), * inputA = new QPushButton();
     addAudio = one->addButton((tr("Add audio in video")), QMessageBox::ActionRole);
     addAudio->setShortcut(Qt::CTRL + Qt::Key_A);
     addAudio->setToolTip("Ctrl+A");
@@ -289,9 +289,12 @@ void MainWindow::on_addRemoveAudio_clicked()
     two->setFont(font);
     two->setIcon(QMessageBox::Icon::Information);
     two->setWindowTitle("Add audio to video or remove audio from it");
-    input = two->addButton((tr("Input file")), QMessageBox::ActionRole);
-    input->setShortcut(Qt::CTRL + Qt::Key_O);
-    input->setToolTip("Ctrl+O");
+    inputV = two->addButton((tr("Input video")), QMessageBox::ActionRole);
+    inputV->setShortcut(Qt::CTRL + Qt::Key_V);
+    inputV->setToolTip("Ctrl+V");
+    inputA = two->addButton((tr("Input audio")), QMessageBox::ActionRole);
+    inputA->setShortcut(Qt::CTRL + Qt::Key_M);
+    inputA->setToolTip("Ctrl+M");
     output = two->addButton((tr("Output folder")), QMessageBox::ActionRole);
     output->setShortcut(Qt::CTRL + Qt::Key_F);
     output->setToolTip("Ctrl+F");
@@ -302,7 +305,57 @@ void MainWindow::on_addRemoveAudio_clicked()
     ok->setShortcut(Qt::CTRL + Qt::Key_K);
     ok->setToolTip("Ctrl+K");
     two->setText("Please select the file and its storage location.");
-
+    QString video, outputFolder, audio;
+    click:
     two->exec();
+    if(two->clickedButton() == inputV)
+    {
+        video = QFileDialog::getOpenFileName(this, tr("Open Video"), "", tr("Video Files(*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p)"));
+        goto click;
+    }
+    else if(two->clickedButton() == inputA)
+    {
+        audio = QFileDialog::getOpenFileName(this, tr("Open Music"), "", tr("Music Files(*.3gp *.mp4 *.m4a *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.m4b *.m4p *.ogg *.oga *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wav *.wma *.webm *.wv *.8svx *.cda)"));
+        goto click;
+    }
+    else if(two->clickedButton() == output)
+    {
+        outputFolder = QFileDialog::getExistingDirectory(this, ("Select Output Folder"), QDir::currentPath());
+        goto click;
+    }
+    else if(two->clickedButton() == ok)
+    {
+        if(video.size() == 0 && outputFolder.size() == 0 && audio.size() == 0)
+        {
+            temp->setText("Files and path not selected.");
+            temp->exec();
+        }
+        else if(outputFolder.size() == 0)
+        {
+            temp->setText("No route selected.");
+            temp->exec();
+        }
+        else if(video.size() == 0)
+        {
+            temp->setText("No video selected.");
+            temp->exec();
+        }
+        else if(audio.size() == 0)
+        {
+            temp->setText("No audio selected.");
+            temp->exec();
+        }
+        else
+        {
+            QString temp = "ffprobe -i ", name;
+            QFileInfo nameTemp(video);
+            name = nameTemp.fileName();
+            temp += video + " -show_entries format=duration -v quiet -of csv=""p=0""";
+            QByteArray temp2 = temp.toLocal8Bit();
+            const char * t2 = temp2.data();
+            //std::cout << t2 << std::endl;
+            system(t2);
+        }
+    }
 
 }
