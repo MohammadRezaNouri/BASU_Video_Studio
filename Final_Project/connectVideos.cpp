666da2bb6501eac4a9b6c6a9957f363d7f5dfc42
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-15 02:45:08 +0430

    temp message box create and set in connectVideos

--- Final_Project/connectVideos.cpp
+++ Final_Project/connectVideos.cpp
@@ -13,7 +13,7 @@ connectVideos::connectVideos()
     QFont font;
     font.setBold(true);
     font.setPointSize(15);
-    QMessageBox * msg = new QMessageBox();
+    QMessageBox * msg = new QMessageBox(), * temp = new QMessageBox();
     msg->setFont(font);
     msg->setIcon(QMessageBox::Icon::Information);
     msg->setText("Please select videos in order of connection type.");
@@ -32,11 +32,18 @@ connectVideos::connectVideos()
     ok->setShortcut(Qt::CTRL + Qt::Key_K);
     ok->setToolTip("Ctrl+K");
     QString outputFolder;
+    QStringList ls;
+    temp->setFont(font);
+    temp->setWindowTitle("Connect several video files to each other");
+    temp->setIcon(QMessageBox::Icon::Critical);
+    ok = temp->addButton(("Ok"), QMessageBox::ActionRole);
+    ok->setShortcut(Qt::CTRL + Qt::Key_K);
+    ok->setToolTip("Ctrl+K");
     click:
     msg->exec();
     if (msg->clickedButton() == inputV)
     {
-        QStringList ls = QFileDialog::getOpenFileNames(nullptr, "Select videos", "/", ("Video Files(*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p)"));
+        ls = QFileDialog::getOpenFileNames(nullptr, "Select videos", "/", ("Video Files(*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p)"));
         goto click;
     }
     else if (msg->clickedButton() == output)
