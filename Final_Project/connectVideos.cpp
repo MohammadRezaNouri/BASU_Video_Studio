5169ba68bd0104cd9fee81ace4b04b1189a6bde9
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-15 02:01:01 +0430

    Making the required message box and buttons as well as setting them and things you want in connectVideos

--- Final_Project/connectVideos.cpp
+++ Final_Project/connectVideos.cpp
@@ -12,4 +12,23 @@ connectVideos::connectVideos()
     QFont font;
     font.setBold(true);
     font.setPointSize(15);
+    QMessageBox * msg = new QMessageBox();
+    msg->setFont(font);
+    msg->setIcon(QMessageBox::Icon::Information);
+    msg->setText("Please select videos in order of connection type.");
+    msg->setWindowTitle("Connect several video files to each other");
+    QPushButton * inputV = new QPushButton(), * output = new QPushButton(), * cancel = new QPushButton(), * ok = new QPushButton();
+    inputV = msg->addButton("Select videos", QMessageBox::ActionRole);
+    inputV->setShortcut(Qt::CTRL + Qt::Key_V);
+    inputV->setToolTip("Ctrl+V");
+    output = msg->addButton("Output folder", QMessageBox::ActionRole);
+    output->setShortcut(Qt::CTRL + Qt::Key_F);
+    output->setToolTip("Ctrl+F");
+    cancel = msg->addButton("Cancel", QMessageBox::ActionRole);
+    cancel->setShortcut(Qt::CTRL + Qt::Key_Q);
+    cancel->setToolTip("Ctrl+Q");
+    ok = msg->addButton("Ok", QMessageBox::ActionRole);
+    ok->setShortcut(Qt::CTRL + Qt::Key_K);
+    ok->setToolTip("Ctrl+K");
+    msg->exec();
 }
