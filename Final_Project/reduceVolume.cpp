29b9f52ea58a8d294c49f0f645733166291aff61
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-16 02:28:16 +0430

    Get crf in reduceVolume

--- Final_Project/reduceVolume.cpp
+++ Final_Project/reduceVolume.cpp
@@ -1,9 +1,16 @@
 #include "reduceVolume.h"
 #include "ui_mainwindow.h"
+#include <QInputDialog>
 
 reduceVolume::reduceVolume()
 {
     QFont font;
     font.setBold(true);
     font.setPointSize(15);
+    bool fOk;
+    int crf = QInputDialog::getInt(nullptr, "Change the volume", "Number : ", QLineEdit::Normal, 0, 51, 5, &fOk);
+    if (!fOk)
+    {
+        return;
+    }
 }
