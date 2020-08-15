0d0d64bde926817aaac8bf4072217a60331c342b
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-15 16:00:02 +0430

    Create font deletePartVideo

--- Final_Project/deletePartVideo.cpp
+++ Final_Project/deletePartVideo.cpp
@@ -1,6 +1,11 @@
 #include "deletePartVideo.h"
+#include "ui_mainwindow.h"
+#include "qmessagebox.h"
 
 deletePartVideo::deletePartVideo()
 {
+    QFont font;
+    font.setBold(true);
+    font.setPointSize(15);
 
 }
