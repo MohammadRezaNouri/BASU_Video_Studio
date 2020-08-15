5d44f87b865f5fa3c8b6e7a57be7adde999ccc90
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-16 02:35:57 +0430

    Create message box and push buttons and seting in reduceVolume

--- Final_Project/reduceVolume.cpp
+++ Final_Project/reduceVolume.cpp
@@ -1,6 +1,7 @@
 #include "reduceVolume.h"
 #include "ui_mainwindow.h"
 #include <QInputDialog>
+#include "qmessagebox.h"
 
 reduceVolume::reduceVolume()
 {
@@ -13,4 +14,23 @@ reduceVolume::reduceVolume()
     {
         return;
     }
+    QMessageBox * msg = new QMessageBox();
+    msg->setWindowTitle("Reduce video volume");
+    msg->setFont(font);
+    msg->setIcon(QMessageBox::Icon::Information);
+    msg->setText("Please select the video and its storage location.");
+    QPushButton * input = new QPushButton(), * output = new QPushButton(), * ok = new QPushButton(), * Cancel = new QPushButton();
+    input = msg->addButton("Input video", QMessageBox::ActionRole);
+    input->setShortcut(Qt::CTRL + Qt::Key_O);
+    input->setToolTip("Ctrl+O");
+    output = msg->addButton("Output folder", QMessageBox::ActionRole);
+    output->setShortcut(Qt::CTRL + Qt::Key_F);
+    output->setToolTip("Ctrl+F");
+    ok = msg->addButton("Ok", QMessageBox::ActionRole);
+    ok->setShortcut(Qt::CTRL + Qt::Key_K);
+    ok->setToolTip("Ctrl+K");
+    Cancel = msg->addButton("Cancel", QMessageBox::ActionRole);
+    Cancel->setShortcut(Qt::CTRL + Qt::Key_Q);
+    Cancel->setToolTip("Ctrl+Q");
+
 }
