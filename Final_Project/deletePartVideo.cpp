13b3b8887243c558b4b507b3f6256d6ef2d8d048
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-15 18:26:42 +0430

    Get end time and check in deletePartVideo

--- Final_Project/deletePartVideo.cpp
+++ Final_Project/deletePartVideo.cpp
@@ -12,9 +12,8 @@ deletePartVideo::deletePartVideo()
     QFont font;
     font.setBold(true);
     font.setPointSize(15);
-    QString sTime, eTime; // Start & end time and select video
     bool fOk;
-    sTime = QInputDialog::getText(nullptr, "Start time to add", "Start time : ", QLineEdit::Normal, "Example : 00:00:00",  &fOk);
+    QString sTime = QInputDialog::getText(nullptr, "Start time to add", "Start time : ", QLineEdit::Normal, "Example : 00:00:00",  &fOk);
     if (!fOk)
     {
         return;
@@ -22,6 +21,7 @@ deletePartVideo::deletePartVideo()
     QMessageBox * temp = new QMessageBox();
     temp->setIcon(QMessageBox::Icon::Critical);
     temp->setWindowTitle("Delete parts of the video");
+    temp->setFont(font);
     if (sTime[0] == '-')
     {
         temp->setText("Start time is not negative.");
@@ -56,6 +56,52 @@ deletePartVideo::deletePartVideo()
         return;
     }
     if (stoi(strVS.substr(6, 2)) > 59)
+    {
+        temp->setText("Wrong input format. (seconds > 59)");
+        temp->exec();
+        delete temp;
+        return;
+    }
+    QString eTime = QInputDialog::getText(nullptr, "End time to add", "End time : ", QLineEdit::Normal, "Example : 00:00:00", &fOk);
+    if (!fOk)
+    {
+        delete temp;
+        return;
+    }
+    if (eTime[0] == '-')
+    {
+        temp->setText("End time is not negative.");
+        temp->exec();
+        delete temp;
+        return;
+    }
+    if (eTime.size() > 8)
+    {
+        temp->setText("Input is incorrect.");
+        temp->exec();
+        delete temp;
+        return;
+    }
+    string strVE = eTime.toStdString();
+    try
+    {
+        int intVE = stoi(strVE.substr(0, 2)) * 3600 + stoi(strVE.substr(3, 2)) * 60 + stoi(strVE.substr(6, 2));
+    }
+    catch(...)
+    {
+        temp->setText("You entered the end date incorrectly.");
+        temp->exec();
+        delete temp;
+        return;
+    }
+    if (stoi(strVE.substr(3, 2)) > 59)
+    {
+        temp->setText("Wrong input format. (minute > 59)");
+        temp->exec();
+        delete temp;
+        return;
+    }
+    if (stoi(strVE.substr(6, 2)) > 59)
     {
         temp->setText("Wrong input format. (seconds > 59)");
         temp->exec();
