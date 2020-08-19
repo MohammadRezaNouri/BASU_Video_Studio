e65a1b4466198e33076b69c087f2858111f53712
Mohammad Reza <MohammadRezaNouri01@Gmail.Com>
2020-08-19 22:23:12 +0430

    Comment for convertFormat files

--- Final_Project/convertFormat.cpp
+++ Final_Project/convertFormat.cpp
@@ -6,19 +6,19 @@
 convertFormat::convertFormat()
 {
     format = QInputDialog::getText(nullptr, "Output format", "Format : ", QLineEdit::Normal, "Example: mp3", &fOk);
-    if (!fOk)
+    if (!fOk) // Cancel button
     {
         return;
     }
-    QString formats = "*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wv *.8svx *.cda)";
-    setWFIOk("Convert video and audio formats");
+    QString formats = "*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wv *.8svx *.cda)"; // True formats
+    setWFIOk("Convert video and audio formats"); // Call dad function
     if (formats.contains(format) == true) // Search input format to all formats
     {
-        setMsgButtons();
+        setMsgButtons(); // Call dad function
         msg->setText("Please select the file and its storage location.");
         click:
         msg->exec();
-        if(msg->clickedButton() == input)
+        if(msg->clickedButton() == input) // Input Video
         {
             fileName = QFileDialog::getOpenFileName(nullptr, "Open Video | Open Music", "", "Video Files | Music Files(*.mp4 *.m4a *.f4v *.f4a *.m4b *.m4r *.f4b *.mov *.3gp *.3gp2 *.3g2 *.3gpp *.3gpp2 *.ogg *.oga *.ogv *.ogx *.wmv *.wma *.asf *.webm *.flv *.mkv *.vob *.drc *.gif *.gifv *.mng *.avi *.MTS *.M2TS *.TS *.qt *.yuv *.tm *.rmvb *.viv *.amv *.m4p *.m4v *.mpg *.mpeg *.mp2 *.mpe *.mpv *.m2v *.svi *.mxf *.roq *.nsv *.f4p *.flac *.mp3 *.wav *.aac *.aa *.aax *.act *.aiff *.alac *.amr *.ape *.au *.awb *.dct *.dss *.dvf *.gsm *.iklax *.ivs *.mmf *.mpc *.msv *.nmf *.mogg *.opus *.ra *.rm *.raw *.rf64 *.sln *.tta *.voc *.vox *.wv *.8svx *.cda)");
             goto click;
@@ -30,7 +30,7 @@ convertFormat::convertFormat()
         }
         else if(msg->clickedButton() == ok)
         {
-            if(fullInput())
+            if(fullInput()) // check input is not empty
             {
                 ffmpeg();
             }
@@ -45,7 +45,7 @@ convertFormat::convertFormat()
 void convertFormat::ffmpeg()
 {
     QString temp = "ffmpeg -i ";
-    if(format == "gif")
+    if(format == "gif") //Because this is different for gifs
     {
         temp += fileName + " -y -vf scale=500:-1 -t 10 -r 10 " + outputFolder + "/" + getName() +"_Convert_format_is_Basu_Video_Studio." + format;
         msgTemp("Please click the OK button and wait for the result page to appear.");
